98af8ecf584387b247e043bd407a8f815692de0f
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-06-01 12:19:03 +1000

    Gives the same result as R code now. Plots histogram.

--- krill-simulation.py
+++ krill-simulation.py
@@ -1,9 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def calc_krill_biomass(survey_area_width, survey_area_length, edsu_width, detected_width):
 
+    n_cells = (survey_area_length/detected_width) * (survey_area_width/edsu_width)
     edsu = np.random.exponential(20, n_cells)
-    edsu.resize([(survey_area_length/edsu_width), (survey_area_width/detected_width)])
+    edsu.resize([(survey_area_length/detected_width), (survey_area_width/edsu_width)])
 
     krill_biomass = np.sum(edsu*detected_width*edsu_width)/1000/1000  # actual biomass in Mt
 
@@ -11,3 +13,12 @@ def calc_krill_biomass(survey_area_width, survey_area_length, edsu_width, detect
 
     return [krill_biomass, calculated_biomass]
 
+krill_biomass = np.zeros(100)
+calculated_biomass = np.zeros(100)
+for i in range(0, 100):
+    run_function = calc_krill_biomass(survey_area_width=6e+05, survey_area_length=2e+05, detected_width=50, edsu_width=2000)
+    krill_biomass[i] = run_function[0]
+    calculated_biomass[i] = run_function[1]
+
+plt.hist(krill_biomass - calculated_biomass)
+plt.show()
