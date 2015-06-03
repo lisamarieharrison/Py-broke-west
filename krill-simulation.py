d7eea601f0817532e289a9d3efab77e61f52204c
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-06-03 14:22:51 +1000

    Added a print of percentage error.

--- krill-simulation.py
+++ krill-simulation.py
@@ -5,7 +5,7 @@ def calc_krill_biomass(survey_area_width, survey_area_length, edsu_width, detect
 
     n_cells = (survey_area_length/detected_width) * (survey_area_width/edsu_width)
     edsu = np.zeros(n_cells)
-    edsu[np.random.choice(range(0, int(n_cells)), size=n_cells*0.95, replace=False)] = np.hstack([np.random.exponential(5, n_cells*0.75), np.random.exponential(100, n_cells*0.2)])
+    edsu[np.random.choice(range(0, int(n_cells)), size=n_cells*0.75, replace=False)] = np.hstack([np.random.exponential(5, n_cells*0.55), np.random.exponential(100, n_cells*0.2)])
     edsu.resize(((survey_area_length/detected_width), (survey_area_width/edsu_width)))
 
     krill_biomass = np.sum(edsu*detected_width*edsu_width)/1e12  # actual biomass in Mt
@@ -19,14 +19,13 @@ n_runs = 200
 krill_biomass = np.zeros(n_runs)
 calculated_biomass = np.zeros(n_runs)
 for i in range(0, n_runs):
-    run_function = calc_krill_biomass(survey_area_width=565e3, survey_area_length=110e3, detected_width=50, edsu_width=2000)
+    run_function = calc_krill_biomass(survey_area_width=565e3, survey_area_length=210e3, detected_width=50, edsu_width=2000)
     krill_biomass[i] = run_function[0]
     calculated_biomass[i] = run_function[1]
 
-plt.hist(krill_biomass - calculated_biomass)
-plt.show()
-
-plt.hist(krill_biomass - distribution_biomass)
-plt.show()
+#plt.hist(krill_biomass - calculated_biomass)
+#plt.show()
 
 print np.mean(krill_biomass)
+
+print np.max(krill_biomass - calculated_biomass)/np.mean(krill_biomass) * 100
