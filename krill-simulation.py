f8da4d74ec89b27fce03eb0838e49421884140b3
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2015-11-25 15:08:50 +1100

    Added plot labels, changed function to include input data which was previously sourced from the global environment and changed error calculation to CV

--- krill-simulation.py
+++ krill-simulation.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def calc_krill_biomass(survey_area_width, survey_area_length, edsu_width, detected_width):
+def calc_krill_biomass(survey_area_width, survey_area_length, edsu_width, detected_width, input_data):
 
     n_cells = (survey_area_length/detected_width) * (survey_area_width/edsu_width)
     # edsu = np.zeros(n_cells)
     # for i in range(1, int(n_cells/50)):
     #     segment_choice = np.random.randint(0, 300)
     #     edsu[(i*50-50):(i*50)] = example_p[segment_choice:(segment_choice+50)]
-    edsu = np.random.choice(example_p, n_cells)
+    edsu = np.random.choice(input_data, n_cells)
     edsu.resize(((survey_area_length/detected_width), (survey_area_width/edsu_width)))
 
     krill_biomass = np.sum(edsu*detected_width*edsu_width)/1e12  # actual biomass in Mt
@@ -26,13 +26,15 @@ calculated_biomass = np.zeros(n_runs)
 example_p = np.genfromtxt('C:/Users/43439535/Documents/Lisa/phd/Mixed models/Data/bw_example_p_3.csv', delimiter=',')
 
 for i in range(0, n_runs):
-    run_function = calc_krill_biomass(survey_area_width=565e3, survey_area_length=210e3, detected_width=50, edsu_width=2000)
+    run_function = calc_krill_biomass(survey_area_width=565e3, survey_area_length=210e3, detected_width=50, edsu_width=2000, input_data=example_p)
     krill_biomass[i] = run_function[0]
     calculated_biomass[i] = run_function[1]
 
 plt.hist(krill_biomass - calculated_biomass)
+plt.xlabel("True - calculated biomass (Mt) for n = 200 simulations")
+plt.ylabel("Frequency")
 plt.show()
 
 print("Mean true biomass (Mt) = " + str(round(np.mean(krill_biomass), 2)))  # mean true biomass in Mt
 
-print("Mean standard deviation of predictions (Mt) = " + str(round(np.std(krill_biomass - calculated_biomass)/np.mean(krill_biomass) * 100, 2)))
+print("CV = " + str(round(np.std(krill_biomass - calculated_biomass)/np.mean(krill_biomass), 2)))
